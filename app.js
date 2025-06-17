fbbb3f663bfe952196bd09d626195cd3450a8554
James Lee <aardvarkpepper@gmail.com>
2025-06-17 11:35:47 -0400

    Add functions to convert date input and current date into years, month, and day numbers in an object.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const formAddTask = document.getElementById('formAddTask');
 
 const hamsterwords = document.getElementById('hamsterwords');
 
-const date = new Date().toLocaleDateString();
+const date = new Date();
 
 formAddTask.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -12,5 +12,25 @@ formAddTask.addEventListener('submit', (event) => {
   const category = formData.get('qscategory');
   const deadline = document.getElementById('deadline').value;
   const status = formData.get('qsstatus');
-  hamsterwords.textContent = `${task}, ${category}, ${deadline}, ${status}; today's date is ${date}`;
+  hamsterwords.textContent = `${task}, ${category}, ${deadline}, ${status}; today's date is ${JSON.stringify(currentDateToObject())} task date is ${JSON.stringify(formDateToObject(deadline))}`;
 })
+
+const formDateToObject = (stringdate) => {
+  return {
+    year: Number(stringdate.slice(0,4)),
+    month: Number(stringdate.slice(5,7)),
+    day: Number(stringdate.slice(8,10)),
+  }
+}
+
+const currentDateToObject = () => {
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+  }
+}
+
+const isTaskOverdue = () => {
+
+}
