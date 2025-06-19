0234f8a50f16acceb3df785e2dbd387db8040cfe
James Lee <aardvarkpepper@gmail.com>
2025-06-19 15:49:20 -0400

    Pare down code before adding.

--- app.js
+++ app.js
@@ -2,39 +2,29 @@ const formAddTask = document.getElementById('formAddTask');
 const taskTable = document.getElementById('taskTable');
 const taskList = [];
 
-document.addEventListener("DOMContentLoaded", function() {
-  hamsterwords.textContent = "On load test successful";
-});
-
-const hamsterwords = document.getElementById('hamsterwords');
-const chinchillawords = document.getElementById('chinchillawords');
-const today = new Date();
+const output1 = document.getElementById('output1');
+const output2 = document.getElementById('output2');
 
 formAddTask.addEventListener('submit', (event) => {
   event.preventDefault();
   // Prevents page reloading, and arguments going to query strings in URL.  Work on a process that uses query strings too.
-  const formData = new FormData(formAddTask);
-  const task = formData.get('qstask');
-  const category = formData.get('qscategory');
-  const deadline = Date(document.getElementById('deadline').value);
-  //populate with current date; but also do validation
-  console.log(`Input:  deadline ${deadline}`)
-  const status = formData.get('qsstatus');
-  hamsterwords.textContent = `${task}, ${category}, ${deadline}, ${status}`;
-  chinchillawords.textContent = `The task is overdue:  ${isTaskOverdue(deadline)} today's date ${today}`
-})
+  const formATData = new FormData(formAddTask);
+  const task = formATData.get('qstask');
+  const category = formATData.get('qscategory');
+  const deadline = formATData.get('deadline');
+  const status = formATData.get('qsstatus');
+
+  output1.textContent = `${task}, ${category}, ${deadline}, ${status}`;
+  // chinchillawords.textContent = `The task is overdue:  ${isTaskOverdue(deadline)} today's date ${today}`
+});
 
 
 
-//in progress, complete, overdue
-//when is this called, really?  On button presses . . . but also when a list is pulled up.  
-// note:  use queryselector too.  but this DOES cause page to reload, doesn't it.
 
-const isTaskOverdue = (deadline) => {
-  const currentDate = new Date();
-  console.log(`isTaskOverdue invoked.  Deadline: ${deadline}.  Current date: ${currentDate}`)
-  return `placeholder`
-}
+/**
+ * 
+ */
+
 const task1 = {
   task: "hamster care",
   category: "Work",
@@ -63,9 +53,34 @@ const task4 = {
   status: "In Progress",
 };
 
-const taskData = [
-];
 
-const addTask = (taskObject) => {
-  taskData.push(taskObject);
-}
+// document.addEventListener("DOMContentLoaded", function() {
+//   // hamsterwords.textContent = "On load test successful";
+// });
+
+// localStorage.setItem("myData", taskList);
+
+// const apple = localStorage.getItem("myData");
+
+
+// const today = new Date();
+
+
+
+
+//in progress, complete, overdue
+//when is this called, really?  On button presses . . . but also when a list is pulled up.  
+// note:  use queryselector too.  but this DOES cause page to reload, doesn't it.
+
+// const isTaskOverdue = (deadline) => {
+//   const currentDate = new Date();
+//   console.log(`isTaskOverdue invoked.  Deadline: ${deadline}.  Current date: ${currentDate}`)
+//   return `placeholder`
+// }
+
+// const taskData = [
+// ];
+
+// const addTask = (taskObject) => {
+//   taskData.push(taskObject);
+// }
