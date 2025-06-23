6344d70a2241d24cfa8779e38e720330f6244ab7
James Lee <aardvarkpepper@gmail.com>
2025-06-22 22:23:10 -0400

    Moved commented code to holder2.js.  Cleaned up app.js.

--- holder2.js
+++ holder2.js
@@ -88,6 +88,7 @@ const addTableHeaders = () => {
     tableHeaderRow.appendChild(tableHeader);
 
     const mrObject = getUniqueElementsInArrayOfObjects(tasklist, ["category", "status"]);
+    console.log(`mrObject; category ${mrObject.category}; status ${mrObject.status}`)
     if (labels[i].textContent === "Category") {
       const mrDrop = createDropdown(mrObject.category, "category", filterStates.category);
       tableHeader.appendChild(mrDrop);
@@ -121,16 +122,30 @@ const createDropdown = (arrayInput, type, defaultOption="All") => {
     } else if (type === "status") {
       filterStates.type = selectedValue;
     }
+    console.log(`Attempting to set ${dropdown.value} to ${selectedValue}`)
+    // const options = Array.from(dropdown.selectedOptions);
+    // for (each of options) {
+    //   console.log(`each: ${each}`)
+    //   each.selected = "selected";
+    // }
     dropdown.value = selectedValue;
+    console.log(`The dropdown has ${[...dropdown.selectedOptions]}`)
+    console.log (`You have selected ${selectedValue} of type ${type}`)
+
+    console.log(`Dropdown AEL activated; current selected value ${selectedValue}`);
 
     if (event.target.value === "All") {
       fillTable(tasklist)
       saveData();
     } else {
+      console.log("INVOKING");
       const arrayOfFilterObjects = [];
       arrayOfFilterObjects.push({[type]: event.target.value});
+      console.log(`INVOKING; ${JSON.stringify(arrayOfFilterObjects)}`);
+
       fillTable(getFilteredArray(tasklist, arrayOfFilterObjects))
       saveData();
+
     }
   });
   return dropdown;
@@ -146,13 +161,17 @@ const createDropdown = (arrayInput, type, defaultOption="All") => {
 const createDropdown2 = (arrayInput, columnName, index) => {
   const dropdown2 = document.createElement("select");
   const valuesArray = ["In Progress", "Complete", "Overdue"];
+  //const objectStorage = getUniqueElementsInArrayOfObjects(tasklist, ["category", "status"]);
+  //console.log(`CD2 ${objectStorage[columnName]}, ${objectStorage[columnName].length}`)
   for (let i = 0; i < valuesArray.length; i++) {
     const option2 = document.createElement("option");
     option2.value = valuesArray[i];
     option2.textContent = valuesArray[i];
     dropdown2.appendChild(option2);
   }
+
   dropdown2.value = tasklist[index].status;
+
   dropdown2.addEventListener('change', (event) => {
     const dropdown2value = event.target.value;
     tasklist[index].status = dropdown2value;
@@ -161,6 +180,8 @@ const createDropdown2 = (arrayInput, columnName, index) => {
   })
   return dropdown2;
 }
+
+
 
 const getFilteredArray = (tasklistInput, arrayOfObjects) => {
   const setOfIndices = new Set();
@@ -179,6 +200,7 @@ const getFilteredArray = (tasklistInput, arrayOfObjects) => {
   for (each of setOfIndices) {
     filteredArray.push(tasklistInput[each]);
   }
+  console.log(`GFA output ${JSON.stringify(filteredArray)}`)
   saveData();
   return filteredArray;
 };
@@ -187,6 +209,7 @@ const isOverdue = (dateInput) => {
   const today = new Date();
   const taskdate = new Date(dateInput)
   output2.textContent = `task: ${taskdate}, today: ${today}`;
+  console.log(`isOverdue testing ${today} and ${taskdate}; ${today > taskdate}`)
   if (today > taskdate) {
     return true;
   } else {
@@ -200,7 +223,9 @@ const addTableTask = (taskObjectParameter, index) => {
   task.textContent = taskObjectParameter.task;
   tableRow.appendChild(task);
   const category = document.createElement("td");
+  //const selectCategory = createDropdown2(tasklist, "category");
   category.textContent = taskObjectParameter.category;
+  // category.appendChild(selectCategory);
   tableRow.appendChild(category);
   const deadline = document.createElement("td");
   deadline.textContent = taskObjectParameter.deadline;
@@ -208,11 +233,16 @@ const addTableTask = (taskObjectParameter, index) => {
   const status = document.createElement("td");
   // If someone wants to mark something overdue when it isn't, perhaps they have their reasons.
   // So no additional handling to correct overdue to in progress.
+  console.log(`For ${taskObjectParameter.task}, boolean is ${isOverdue(taskObjectParameter.deadline)}`)
   if (isOverdue(taskObjectParameter.deadline) && taskObjectParameter.status === "In Progress") {
+    if (taskObjectParameter.task === "hamster care") {
+      console.log(`Triggered hamster care.`)
+    }
     taskObjectParameter.status = "Overdue";
     tasklist[index].status = "Overdue";
     saveData();
   }
+  console.log(`Attempting to generate for ${taskObjectParameter.task}, ${taskObjectParameter.category}, ${taskObjectParameter.deadline}, ${taskObjectParameter.status}.`);
   status.textContent = taskObjectParameter.status;
   const dropdown2 = createDropdown2(tasklist, "status", index);
   status.appendChild(dropdown2);
@@ -222,19 +252,74 @@ const addTableTask = (taskObjectParameter, index) => {
 
 const fillTable = (arrayOfTaskObjects) => {
   clearTable();
+  if (document.getElementById("typeWork")){
+    console.log('Detect');
+  }
   addTableHeaders();
   for (let i = 0; i < arrayOfTaskObjects.length; i++) {
     addTableTask(arrayOfTaskObjects[i], i);
   }
 }
 
+/**
+ * persist task data using local storage
+ * 
+ */
+
+const task1 = {
+  task: "hamster care 2",
+  category: "Work",
+  deadline: "2025-06-19",
+  status: "In Progress",
+};
+
+const task2 = {
+  task: "past care",
+  category: "Pastries",
+  deadline: "2024-01-02",
+  status: "In Progress", // In Progress
+};
+
+const task3 = {
+  task: "hamster care",
+  category: "Work",
+  deadline: "2028-11-12",
+  status: "Complete", // Overdue
+};
+
+const task4 = {
+  task: "Past Care",
+  category: "Duplicates",
+  deadline: "2025-06-19",
+  status: "Complete",
+};
+
+// tasklist.push(task1);
+// tasklist.push(task2);
+// tasklist.push(task3);
+// tasklist.push(task4);
+
+
 // Runs contents on page load.
 document.addEventListener("DOMContentLoaded", function() {
   retrieveData();
   fillTable(tasklist);
+  // hamsterwords.textContent = "On load test successful";
 });
 
+// localStorage.setItem("myData", tasklist);
+
 /**
+ * To do:
+ * 
+ * Update the status of tasks to reflect their progress (e.g., “In Progress,” “Completed,” “Overdue”).
+ * via dropdown or button.  Tasklist should update when this is done.
+ * 
+ * Inside each row of the table, I have dropdowns with an onChange event listener. When it is changed,
+ * the data is updated in js then the table re-rendered.  I expect filters should stay.
+ * 
+ * 
+ * Persist task data using local storage so tasks are saved even after refreshing the page.
  * 
  * Note:  After closing server and re-opening, data persists and error does not recur.  This is expected.  To replicate error, though, must clear data with
  * 
